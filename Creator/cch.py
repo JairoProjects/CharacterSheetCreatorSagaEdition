2187eb13ebc7a1f06a2cc555342e4808061970e1
JairoProjects <jairo.echegoyen@gmail.com>
2022-01-26 09:33:01 +0100

    Creado el archivo cch.py, que permite elegir una clase.

---
+++ Creator/cch.py
@@ -0,0 +1,25 @@
+﻿class Charclass():
+	def __init__(self, name):
+		self.name=name
+		self.refbonus=0
+		self.fortbonus=0
+		self.wilbonus=0
+		self.setBonus()
+
+	def setBonus(self):
+		if self.name=="Jedi":
+			self.refbonus=1
+			self.fortbonus=1
+			self.wilbonus=1
+		if self.name=="Noble":
+			self.refbonus=1
+			self.wilbonus=2
+		if self.name=="Granuja":
+			self.refbonus=2
+			self.wilbonus=1
+		if self.name=="Explorador":
+			self.refbonus=2
+			self.fortbonus=1
+		if self.name=="Soldado":
+			self.refbonus=1
+			self.fortbonus=2
