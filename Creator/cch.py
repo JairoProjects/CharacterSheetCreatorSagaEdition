10a244aea5bd3d2dda31b24b2be5d2b23b1991ce
JairoProjects <jairo.echegoyen@gmail.com>
2022-03-02 10:39:46 +0100

    Añadida funcionalidad para calcular el ataque base. Se ha modificado el archivo cch.py para aceptar la nueva funcionalidad

--- Creator/cch.py
+++ Creator/cch.py
@@ -1,9 +1,13 @@
-﻿class Charclass():
-	def __init__(self, name):
+﻿from setattack import setAttackWhenAttackIsEqualsLevel, setAttackWhenAttackIsNotEqualsLevel
+
+class Charclass():
+	def __init__(self, name, level):
 		self.name=name
+		self.level=level
 		self.refbonus=0
 		self.fortbonus=0
 		self.wilbonus=0
+		self.attackbonus=0
 		self.setBonus()
 
 	def setBonus(self):
@@ -11,15 +15,20 @@
 			self.refbonus=1
 			self.fortbonus=1
 			self.wilbonus=1
+			self.attackbonus=setAttackWhenAttackIsEqualsLevel(self.level)
 		if self.name=="Noble":
 			self.refbonus=1
 			self.wilbonus=2
+			self.attackbonus=setAttackWhenAttackIsNotEqualsLevel(self.level)
 		if self.name=="Granuja":
 			self.refbonus=2
 			self.wilbonus=1
+			self.attackbonus=setAttackWhenAttackIsNotEqualsLevel(self.level)
 		if self.name=="Explorador":
 			self.refbonus=2
 			self.fortbonus=1
+			self.attackbonus=setAttackWhenAttackIsNotEqualsLevel(self.level)
 		if self.name=="Soldado":
 			self.refbonus=1
 			self.fortbonus=2
+			self.attackbonus=setAttackWhenAttackIsEqualsLevel(self.level)
---
+++ Creator/setattack.py
@@ -0,0 +1,16 @@
+﻿def setAttackWhenAttackIsEqualsLevel(level):
+	attack=level
+	return attack
+
+def setAttackWhenAttackIsNotEqualsLevel(level):
+	if level>=0 and level<=4:
+		attack=level-1
+	if level>=5 and level<=8:
+		attack=level-2
+	if level>=9 and level<=12:
+		attack=level-3
+	if level>=13 and level<=16:
+		attack=level-4
+	if level>=17 and level<=20:
+		attack=level-5
+	return attack
